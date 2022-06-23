7fe8b1ec827fffece8e2bf9c9afdefbf59de100b
¨mago¨ <martingramajo08@gmail.com>
2022-06-23 02:45:09 -0300

    agrego comentario en memesRoute

--- routes/memesRoute.js
+++ routes/memesRoute.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const memeControllers = require('../controllers/memeController');
 
 
-router.post('/', memeControllers.crearMeme); 
-router.get('/', memeControllers.obtenerMemes);
-router.get('/:id', memeControllers.obtenerMeme);
-router.put('/:id', memeControllers.modificarMeme);
-router.delete('/:id', memeControllers.borrarMeme);
+router.post('/', memeControllers.crearMeme); // Para crear meme.
+router.get('/', memeControllers.obtenerMemes); // Listar memes.
+router.get('/:id', memeControllers.obtenerMeme); // Buscar un meme en especifico con su id.
+router.put('/:id', memeControllers.modificarMeme); // Modificar meme.
+router.delete('/:id', memeControllers.borrarMeme); // Eliminar meme. Validar creador antes de eliminar. 
 module.exports = router;
